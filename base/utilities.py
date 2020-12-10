05b94562e15b0dfad42ee14938997676854432e7
Monesh kovi <67748834+moneshkovi@users.noreply.github.com>
2020-12-10 16:32:07 +0530

    Add files via upload

---
+++ base/utilities.py
@@ -0,0 +1,138 @@
+from __future__ import annotations
+from io import BytesIO
+from PIL import Image, ImageDraw, ImageFont
+from PIL import Image, ImageOps, ImageDraw
+import random
+import asyncpg
+import ssl
+ctx = ssl.create_default_context(cafile='rds-combined-ca-bundle.pem')
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+class Database:
+    def __init__(self, loop, user: str, password: str) -> None:
+        self.user = user
+        self.password = password
+        loop.create_task(self.connect())
+
+    async def connect(self) -> None:
+        self.conn = await asyncpg.connect(user=self.user, password=self.password, database='d9m1ilmuvnuaka', host='ec2-52-20-66-171.compute-1.amazonaws.com',ssl=ctx)
+        await self.conn.close()
+        self.conn = await asyncpg.connect(user=self.user, password=self.password, database='d9m1ilmuvnuaka', host='ec2-52-20-66-171.compute-1.amazonaws.com',ssl=ctx)
+        await self.conn.fetch('CREATE TABLE IF NOT EXISTS users (id TEXT NOT NULL, rank INT NOT NULL, xp INT NOT NULL)')
+
+    async def fetch(self, sql: str) -> list:
+        return await self.conn.fetch(sql)
+
+
+class Rank:
+    def __init__(self) -> None:
+        self.font = ImageFont.truetype('arialbd.ttf', 42)
+        self.medium_font = ImageFont.truetype('arial.ttf', 34)
+        self.medium_bold_font = ImageFont.truetype('arialbd.ttf', 34)
+        self.small_font = ImageFont.truetype('arialbd.ttf', 24)
+        self.rank_font = ImageFont.truetype('arialbd.ttf', 28)
+        self.very_small_font = ImageFont.truetype('arialbd.ttf',22)
+    def draw(self, user: str, rank: str, xp: str, profile_bytes: BytesIO) -> BytesIO:
+        profile_bytes = Image.open(profile_bytes)
+        bg = Image.open('codm.png')
+        bg.resize((100,100))
+
+        profile_bytes = profile_bytes.resize((120,120), Image.ANTIALIAS)
+        bigsize = (profile_bytes.size[0] * 3, profile_bytes.size[1] * 3)
+        mask = Image.new('L', bigsize, 0)
+        draw = ImageDraw.Draw(mask)
+        draw.ellipse((0, 0) + bigsize, fill=255)
+        mask = mask.resize(profile_bytes.size, Image.ANTIALIAS)
+        profile_bytes.putalpha(mask)
+        output = ImageOps.fit(profile_bytes, mask.size, centering=(0.5, 0.5))
+        output.putalpha(mask)
+        im = Image.new('RGBA', (650, 850), (44, 44, 44, 255))
+        im.paste(bg, (0, 0), bg)
+        im_draw = ImageDraw.Draw(im)
+        w, h = im_draw.textsize(user, font=self.rank_font)
+        print(w)
+        im_draw.text((250+(200-w)/2, 330), user, font=self.rank_font, fill=(255, 255, 255, 255))
+
+        rank_text = f'RANK {rank}'
+        im_draw.text((100, 360), rank_text, font=self.rank_font, fill=(255, 255, 255, 255))
+
+        needed_xp = self.neededxp(rank)
+        xp_text = f'{xp}/{needed_xp}'
+        im_draw.text((300, 380), xp_text, font=self.small_font, fill=(255, 255, 255, 255))
+
+        im_draw.rectangle((250, 380, 450, 370), fill=(64, 64, 64, 255))
+        im_draw.rectangle((250, 380, 250+(int(xp/needed_xp*100))*2, 370), fill=(221, 221, 221, 255))
+        im_draw.rectangle((250+(int(xp/needed_xp*100))*2, 380, 250+(int(xp/needed_xp*100))*2+3, 370), fill=(255, 0, 0, 255))
+        im_draw.text((0, 480), "Level Role", font=self.font, fill=(255, 255, 255, 255))
+        im_draw.text((0, 600), "Next Reward", font=self.font, fill=(255, 255, 255, 255))
+        linew = 770
+        quotes = ["if you want it, work for it.", "Nothing Lasts forever.", "No guts, no story.", "Stay hungry.Stay foolish", "Dream big.Pray Bigger.", "Fight till the last gasp", " Enjoy the little things", " Keep going. Be all in. ", " Screw it, let's do it. ", " Take the risk or loose the chance. ",  "Feel the fear & do it anyway. ", " Never stop dreaming. "]
+        if rank < 5:
+            im_draw.text((0, 540), "No Level Reached Yet", font=self.medium_font, fill=(255, 255, 255, 255))
+            im_draw.text((0, 660), "Enthusiast", font=self.medium_font, fill=(255, 255, 255, 255))
+            im_draw.text((0, 720), "• Ability to post media in media channel", font=self.very_small_font, fill=(255, 255, 255, 255))
+            #im_draw.text((0, 720), "• Reward 2", font=self.medium_font, fill=(255, 255, 255, 255))
+        elif rank >= 5 and rank < 15:
+            im_draw.text((0, 540), "Enthusiast", font=self.medium_font, fill=(255, 255, 255, 255))
+            im_draw.text((0, 660), "Rookie", font=self.medium_font, fill=(255, 255, 255, 255))
+            im_draw.text((0, 720), "• Ability to use External emojies", font=self.very_small_font, fill=(255, 255, 255, 255))
+            im_draw.text((0, 750), "• Obtain a coloured name", font=self.very_small_font, fill=(255, 255, 255, 255))
+        elif rank >= 15 and rank < 25:
+            im_draw.text((0, 540), "Rookie", font=self.medium_font, fill=(255, 255, 255, 255))
+            im_draw.text((0, 660), "Veterain", font=self.medium_font, fill=(255, 255, 255, 255))
+            im_draw.text((0, 720), "• Ability to react messages", font=self.very_small_font, fill=(255, 255, 255, 255))
+            #im_draw.text((0, 720), "• Reward 2", font=self.very_small_font, fill=(255, 255, 255, 255))
+        elif rank >= 25 and rank < 35:
+            im_draw.text((0, 540), "Veterain", font=self.medium_font, fill=(255, 255, 255, 255))
+            im_draw.text((0, 660), "Elite", font=self.medium_font, fill=(255, 255, 255, 255))
+            im_draw.text((0, 720), "• Ability to share scrren & live stream", font=self.very_small_font, fill=(255, 255, 255, 255))
+            #im_draw.text((0, 720), "• Reward 2", font=self.very_small_font, fill=(255, 255, 255, 255))
+        elif rank >= 35 and rank < 50:
+            im_draw.text((0, 540), "Elite", font=self.medium_font, fill=(255, 255, 255, 255))
+            im_draw.text((0, 660), "Pro", font=self.medium_font, fill=(255, 255, 255, 255))
+            im_draw.text((0, 720), "• Ability to change your Nickname", font=self.very_small_font, fill=(255, 255, 255, 255))
+            #im_draw.text((0, 720), "• Reward 2", font=self.very_small_font, fill=(255, 255, 255, 255))
+        elif rank >= 50 and rank < 65:
+            im_draw.text((0, 540), "Pro", font=self.medium_font, fill=(255, 255, 255, 255))
+            im_draw.text((0, 660), "Master", font=self.medium_font, fill=(255, 255, 255, 255))
+            im_draw.text((0, 720), "• Ability to share media files in chat", font=self.very_small_font, fill=(255, 255, 255, 255))
+            #im_draw.text((0, 720), "• Reward 2", font=self.very_small_font, fill=(255, 255, 255, 255))
+        elif rank >= 65 and rank < 75:
+            im_draw.text((0, 540), "Master", font=self.medium_font, fill=(255, 255, 255, 255))
+            im_draw.text((0, 660), "Legendary", font=self.medium_font, fill=(255, 255, 255, 255))
+            im_draw.text((0, 720), "• Ability to DJ Commands", font=self.very_small_font, fill=(255, 255, 255, 255))
+            im_draw.text((0, 750), "• Access to VIP Channels(Giveaways)", font=self.very_small_font, fill=(255, 255, 255, 255))
+        elif rank >= 75 and rank < 85:
+            im_draw.text((250+(200-w)/2, 330), user, font=self.rank_font, fill=(255, 0, 0, 255))
+            im_draw.text((0, 540), "Legendary", font=self.medium_font, fill=(255, 255, 255, 255))
+            im_draw.text((0, 660), "Mythical", font=self.medium_font, fill=(255, 255, 255, 255))
+            im_draw.text((0, 720), "• Ability to post GIF links", font=self.very_small_font, fill=(255, 255, 255, 255))
+            #im_draw.text((0, 720), "• Reward 2", font=self.very_small_font, fill=(255, 255, 255, 255))
+        else:
+            im_draw.text((250+(200-w)/2, 330), user, font=self.rank_font, fill=(255, 0, 0, 255))
+            im_draw.text((0, 540), "Mythical", font=self.medium_font, fill=(255, 255, 255, 255))
+            im_draw.text((0, 660), "Highest Reward Reached", font=self.medium_font, fill=(255, 255, 255, 255))
+            im_draw.text((0, 720), "• No Reward", font=self.very_small_font, fill=(255, 255, 255, 255))
+        im_draw.line((0, 790, 650, 790),fill=(255,255,255,255),width=2)
+        quote = random.choice(quotes)
+        im_draw.text((20, 810), "“"+quote+"”", font=self.very_small_font, fill=(255, 255, 255, 255))
+        im.paste(profile_bytes, (270, 200), profile_bytes)
+
+        buffer = BytesIO()
+        im.save(buffer, 'png')
+        buffer.seek(0)
+
+        return buffer
+
+    @staticmethod
+    def neededxp(level: str) -> int:
+        return 100+level*80
+
+
+class Utilities:
+    def __init__(self):
+        self.database = Database
+        self.rankcard = Rank()
+
+
+utilities = Utilities()
---
+++ cogs/leveling.py
@@ -0,0 +1,137 @@
+from __future__ import annotations
+from asyncio import sleep as asyncsleep
+from base.utilities import utilities
+from discord.ext import commands
+from random import randint
+from io import BytesIO
+from discord import File as dFile
+from discord import Member as dMember
+import aiohttp
+import discord
+import re
+class Leveling(commands.Cog):
+    def __init__(self, bot) -> None:
+        self.bot = bot
+        self.db = utilities.database(self.bot.loop, self.bot.cfg.postgresql_user, self.bot.cfg.postgresql_password)
+        self.brake = []
+    @commands.Cog.listener()
+    async def on_message(self, message) -> None:
+        if message.author == self.bot:
+            return
+        if message.author.id == 758292526486126632:
+            return
+        result = await self.db.fetch(f'SELECT rank, xp FROM users WHERE id=\'{message.author.id}\'')
+        Enthusiast = discord.utils.get(message.guild.roles, name = "Enthusiast")
+        Rookie = discord.utils.get(message.guild.roles, name = "Rookie")
+        Veterain = discord.utils.get(message.guild.roles, name = "Veterain")
+        Elite = discord.utils.get(message.guild.roles, name = "Elite")
+        Pro = discord.utils.get(message.guild.roles, name = "Pro")
+        Master = discord.utils.get(message.guild.roles, name = "Master")
+        Legendary = discord.utils.get(message.guild.roles, name = "Legendary")
+        Mythical = discord.utils.get(message.guild.roles, name="Mythical")
+        if result[0][0] >= 5 and Enthusiast not in message.author.roles:
+            await message.author.add_roles(Enthusiast)
+            await message.channel.send(f"{message.author.mention} has been promoted to Enthusiast")
+        elif result[0][0] >=15 and Rookie not in message.author.roles:
+            await message.author.add_roles(Rookie)
+            await message.channel.send(f"{message.author.mention} has been promoted to Rookie")
+        elif result[0][0] >=25 and Veterain not in message.author.roles:
+            await message.author.add_roles(Veterain)
+            await message.channel.send(f"{message.author.mention} has been promoted to Veterain")
+        elif result[0][0] >=35 and Elite not in message.author.roles:
+            await message.author.add_roles(Elite)
+            await message.channel.send(f"{message.author.mention} has been promoted to Elite")
+        elif result[0][0] >=50 and Pro not in message.author.roles:
+            await message.author.add_roles(Pro)
+            await message.channel.send(f"{message.author.mention} has been promoted to Pro")
+        elif result[0][0] >=65 and Master not in message.author.roles:
+            await message.author.add_roles(Master)
+            await message.channel.send(f"{message.author.mention} has been promoted to Master")
+        elif result[0][0] >=75 and Legendary not in message.author.roles:
+            await message.author.add_roles(Legendary)
+            await message.channel.send(f"{message.author.mention} has been promoted to Legendary")
+        elif result[0][0] >=85 and Mythical not in message.author.roles:
+            await message.author.add_roles(Mythical)
+            await message.channel.send(f"{message.author.mention} has been promoted to Mythical")
+
+        if message.author.id not in self.brake:
+            if not await self.db.fetch(f'SELECT * FROM users WHERE id=\'{message.author.id}\''):
+                await self.db.fetch(f'INSERT INTO users (id, rank, xp) VALUES (\'{message.author.id}\', \'0\', \'0\')')
+                current_xp = 0
+
+            else:
+                result = await self.db.fetch(f'SELECT rank, xp FROM users WHERE id=\'{message.author.id}\'')
+                current_xp = result[0][1] + randint(self.bot.cfg.min_message_xp, self.bot.cfg.max_message_xp)
+
+                if current_xp >= utilities.rankcard.neededxp(result[0][0]):
+                    await self.db.fetch(f'UPDATE users SET rank=\'{result[0][0]+1}\', xp=\'0\' WHERE id=\'{message.author.id}\'')
+                else:
+                    await self.db.fetch(f'UPDATE users SET xp=\'{current_xp}\' WHERE id=\'{message.author.id}\'')
+                self.brake.append(message.author.id)
+                await asyncsleep(randint(15, 25))
+                self.brake.remove(message.author.id)
+    @commands.Cog.listener()
+    async def on_member_join(self, member):
+        embed=discord.Embed(title=f"Hey {member.name}, Welcome to Call of Duty: Mobile!", description="""Make sure you go through #🎄-information-desk and have a read through all the rules and follow them to avoid any infractions.\n
+Call of Duty: Mobile is a free-to-play first-person shooter game developed by TiMi Studios and published by Activision. Being developed for mobile platform, this game brings together the maps, weapons and characters from across the Call of Duty series in the definitive first-person action experience on mobile.\n
+As of right now, we are gathering information regarding the overall player experience to help make changes and improvements for the same. Please do fill up this survey form to help make the game better for our audience.\n
+This server is a community-run server, owned by TiMi Studios and supported by Activision, so feel free to address and express your concerns for the game to help improve it or just to have a wonderful time!\n
+So, what are you waiting for? See ya in the Battlefield!\n """, color=0x00ff00)
+        file = discord.File("c.jpg", filename="image.jpg")
+        embed.set_image(url="attachment://image.jpg")
+        await member.send(file=file, embed=embed)
+
+    @commands.command()
+    async def help(self, ctx) -> None:
+        embed=discord.Embed(title=f"Hey {ctx.author.name}, Here are the commands", description="""Leveling Commands
+**\n.rank**
+Displays your current level with an intuitive layout.
+Aliases: **.rank** \n\n**.leaderboard**
+Displays the top 10 most active users in the server!
+Aliases: **.lb**\n \n**.color**
+Set a color of your own to be displayed with your rank card.
+Aliases: **.color**\n """, color=0x00ff00)
+        await ctx.send(embed=embed)
+
+    @commands.command()
+    async def lb(self, ctx) -> None:
+        result = await self.db.fetch(f'SELECT id, rank, xp FROM users ORDER BY rank desc LIMIT 10 ')
+        for i in result:
+            if i[0] == 741306813965795430:
+                result.remove(i)
+
+        embed=discord.Embed(title=f"leaderboard",  color=0x00ff00)
+        counter = 1
+        for i in result:
+            try:
+                user = await ctx.guild.fetch_member(i[0])
+                if user != None:
+                    embed.add_field(name=str(counter)+"-"+user.name, value="🌟 Level : "+str(i[1])+"\n"+"EXP: "+str(i[2])+"/"+str(i[2]*80+100), inline=False)
+                    counter += 1
+            except:
+                continue
+        await ctx.send(embed=embed)
+    @commands.command()
+    async def rank(self, ctx, member: dMember=None) -> None:
+        if member:
+            uMember = member
+        else:
+            uMember = ctx.author
+        result = await self.db.fetch(f'SELECT rank, xp FROM users WHERE id=\'{uMember.id}\'')
+        if result:
+            async with aiohttp.ClientSession() as session:
+                async with session.get(f'{uMember.avatar_url}?size=128') as resp:
+                    profile_bytes = await resp.read()
+
+            buffer = utilities.rankcard.draw(str(uMember), result[0][0], result[0][1], BytesIO(profile_bytes))
+
+            await ctx.send(file=dFile(fp=buffer, filename='rank_card.png'))
+        else:
+            await ctx.send(f'{uMember.mention}, you don\'t received xp yet.')
+
+
+def setup(bot) -> None:
+    bot.add_cog(Leveling(bot))
+"""**.background**\n
+Choose a custom background from the gallery of images available for your rank card!
+Aliases: **.bg, .setbg**"""
