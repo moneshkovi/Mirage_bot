6ba824d8b7fd02e65ca0378e679a76bf8e241177
Tron <67748834+moneshkovi@users.noreply.github.com>
2022-09-10 09:19:31 +0530

    Update struct.py

--- base/struct.py
+++ base/struct.py
@@ -7,6 +7,6 @@ class Config:
         self.postgresql_user = cfg['postgresql_user']
         self.postgresql_password = cfg['postgresql_password']
         self.min_message_xp = cfg['min_message_xp']
-        self.max_message_xp = cfg['max_message_xp']
+        self.max_message_xp = cfg['max_message_xp']   
 
         
